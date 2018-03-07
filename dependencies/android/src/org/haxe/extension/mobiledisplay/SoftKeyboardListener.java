3ed9774ce110046348fb184fd2adcfa44e4c2122
marlus <marluscadanus@gmail.com>
2018-03-07 15:34:39 -0500

    [FIX] Created a function to resize the at first time.

--- dependencies/android/src/org/haxe/extension/mobiledisplay/SoftKeyboardListener.java
+++ dependencies/android/src/org/haxe/extension/mobiledisplay/SoftKeyboardListener.java
@@ -13,7 +13,7 @@ public class SoftKeyboardListener
     private boolean m_isSoftKeyboardVisible = false;
     
     public SoftKeyboardListener()
-    {        
+    {
     }
     
     public void InstallKeyboardListener(final View activityRootView)
@@ -21,41 +21,45 @@ public class SoftKeyboardListener
         activityRootView.getViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
-                Rect r = new Rect();
-                
-                //r will be populated with the coordinates of the view area still visible
-                activityRootView.getWindowVisibleDisplayFrame(r);
-                int screenWidth = activityRootView.getRootView().getWidth();
-                int screenHeight = activityRootView.getRootView().getHeight() - r.top;
-                final int heightDiff = screenHeight - (r.bottom - r.top);
-                
-                //Utils.LogInfo("Rect: left=" + r.left + ", top=" + r.top + ", right=" + r.right + ", bottom=" + r.bottom);
-                //Utils.LogInfo("screenHeight: " + screenHeight);
-                //Utils.LogInfo("heightDiff: " + heightDiff);
-                //Utils.LogInfo("keyboardVisible: " + m_isSoftKeyboardVisible);
-                
-                if (heightDiff > 0) {
-                    m_isSoftKeyboardVisible = true;
-                    HaxeCallback.DispatchEventToHaxe(
-                        "mobiledisplay.event.MobileKeyboardPopupEvent", 
-                        new Object[] {
-                            "mobiledisplay_keyboard_activated", 
-                            screenWidth,
-                            screenHeight,
-                            heightDiff
-                        });
-                } else if (heightDiff <= 0) {
-                    m_isSoftKeyboardVisible = false;
-                    HaxeCallback.DispatchEventToHaxe(
-                            "mobiledisplay.event.MobileKeyboardPopupEvent", 
-                            new Object[] {
-                                "mobiledisplay_keyboard_deactivated", 
-                                screenWidth,
-                                screenHeight,
-                                heightDiff
-                            });
-                }
+                resize(activityRootView);
             }
         });
+    }
+
+    public void resize(final View activityRootView) {
+        Rect r = new Rect();
+        
+        //r will be populated with the coordinates of the view area still visible
+        activityRootView.getWindowVisibleDisplayFrame(r);
+        int screenWidth = activityRootView.getRootView().getWidth();
+        int screenHeight = activityRootView.getRootView().getHeight() - r.top;
+        final int heightDiff = screenHeight - (r.bottom - r.top);
+        
+        //Utils.LogInfo("Rect: left=" + r.left + ", top=" + r.top + ", right=" + r.right + ", bottom=" + r.bottom);
+        //Utils.LogInfo("screenHeight: " + screenHeight);
+        //Utils.LogInfo("heightDiff: " + heightDiff);
+        //Utils.LogInfo("keyboardVisible: " + m_isSoftKeyboardVisible);
+        
+        if (heightDiff > 0) {
+            m_isSoftKeyboardVisible = true;
+            HaxeCallback.DispatchEventToHaxe(
+                "mobiledisplay.event.MobileKeyboardPopupEvent", 
+                new Object[] {
+                    "mobiledisplay_keyboard_activated", 
+                    screenWidth,
+                    screenHeight,
+                    heightDiff
+                });
+        } else if (heightDiff <= 0) {
+            m_isSoftKeyboardVisible = false;
+            HaxeCallback.DispatchEventToHaxe(
+                    "mobiledisplay.event.MobileKeyboardPopupEvent", 
+                    new Object[] {
+                        "mobiledisplay_keyboard_deactivated", 
+                        screenWidth,
+                        screenHeight,
+                        heightDiff
+                    });
+        }
     }
 }
